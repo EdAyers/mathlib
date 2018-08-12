3fc6ab4255fe47761294a244a05b8d91b339f70e
E.W.Ayers <E.W.Ayers@maths.cam.ac.uk>
2018-08-12 23:29:32 +0100

    Rewrite to use `decidable_linear_order`

--- data/tree23.lean
+++ data/tree23.lean
@@ -8,7 +8,7 @@ inductive node (k : Type u) (α : Type u) : ℕ -> Type u
 |Two {n}: node n -> (k × α) -> node n -> node (n + 1)
 |Three {n}: node n -> (k × α) -> node n -> (k × α) -> node n -> node (n + 1)
 namespace node
-variables {k : Type u} [linear_order k]
+variables {k : Type u} [decidable_linear_order k]
 variables {α β δ: Type u}
 def mem : Π {n}, (k) -> (node k α n) -> Prop
 |(0) _ Empty := false
@@ -35,7 +35,7 @@ def minkey : Π {n}, node k α (n+1) -> k := λ n t, prod.fst (min t : k × α)
 
 open ordering
 
-def is_ordered : Π {n}, node k α n -> Prop 
+@[simp] def is_ordered : Π {n}, node k α n -> Prop 
 |(0) Empty := true
 |(1) (Two Empty _ Empty) := true
 |(1) (Three Empty ⟨k1,_⟩ Empty ⟨k2,_⟩ Empty) := k1 < k2
@@ -48,12 +48,19 @@ def is_ordered : Π {n}, node k α n -> Prop
     ∧ (is_ordered  m) ∧ ( k1 < (minkey m)) ∧ ((maxkey m) < k2)
     ∧ (is_ordered  r) ∧ ( k2 < (minkey r))
 
-variables (lt : k -> k -> Prop) [decidable_rel lt]
-def empty : node k α 0 := node.Empty
-def map (f : k -> α -> β)  : Π {n}, node k α n -> node k β n
+@[simp] def empty : node k α 0 := node.Empty
+@[simp] def map (f : k -> α -> β)  : Π {n}, node k α n -> node k β n
 |0 Empty := Empty
 |n (Two l ⟨k,a⟩ r) := Two (map l) ⟨k, f k a⟩ (map r)
 |n (Three l ⟨k1,a1⟩ m ⟨k2,a2⟩ r) := Three (map l) ⟨k1,f k1 a1⟩ (map m) ⟨k2,f k2 a2⟩ (map r)
+
+lemma map_preserves_ordering {n : ℕ} (f : k -> α -> β) (t : node k α 0) 
+: is_ordered t -> is_ordered (map f t) 
+:= begin 
+  intro H1,
+  cases t,
+  simp *
+end
 
 def fold (f : k -> α -> β -> β) : Π {n}, node k α n -> β -> β
 |0 Empty b := b
@@ -70,6 +77,7 @@ def traverse {m : Type u → Type u} [applicative m] {α β : Type u} (f : k -> 
     pure (λ l b1 m b2 r, Three l ⟨k1,b1⟩ m ⟨k2, b2⟩ r) 
     <*> traverse l <*> f k1 a1 <*> traverse m <*> f k2 a2 <*> traverse r
 
+-- TODO; what does an ordering proof look like for traverse?
 
 def find (p : k -> α -> bool): Π {n}, node k α n -> option (k × α)
 |0 Empty := none
@@ -81,28 +89,23 @@ def find (p : k -> α -> bool): Π {n}, node k α n -> option (k × α)
     <|> (if (p k2 a2) then some ⟨k2,a2⟩ else none)
     <|> find r
 
-
-
 def get (i : k): Π {n}, node k α n -> option α
 |0 Empty := none
 |n (Two l ⟨k,a⟩ r) :=
-    match cmp_using lt i k with
-    |ordering.lt := get l
-    |ordering.eq := some a
-    |ordering.gt := get r
-    end
+    if i < k then get l
+    else if i = k then some a
+    else get r
 |n (Three l ⟨k1,a1⟩ m ⟨k2,a2⟩ r) := 
-    match cmp_using lt i k1 with
-    |ordering.lt := get l 
-    |ordering.eq := some a1
-    |ordering.gt := match cmp_using lt i k2 with
-           |ordering.lt := get m
-           |ordering.eq := some a2
-           |ordering.gt := get r
-           end
-    end
+    if i < k1 then get l
+    else if i = k1 then some a1
+    else if i < k2 then get m
+    else if i = k2 then some a2
+    else get r
 
-def has {n} (i : k) : node k α n -> bool := option.is_some ∘ get lt i
+def has {n} (i : k) : node k α n -> bool := option.is_some ∘ get i
+
+lemma is_ordered_implies_has_implies_mem {n:ℕ} (t : node k α n) (i : k) (io : is_ordered t)
+: (i ∈ t) <-> ((has i t)) := by sorry
 
 section
     inductive growth (k : Type u) (α : Type u) : ℕ -> Type u
@@ -112,60 +115,45 @@ section
     private def modify_raw (i : k) (f : option α -> α) : Π {n}, node k α n -> growth k α n
     |0 Empty := Sprout Empty (i, f none) Empty
     |(n+1) (Two l (p@⟨k,a⟩) r) :=
-        match cmp_using lt i k with
-        |ordering.lt :=
+        if i < k then 
             match modify_raw l with
             |(Stay l) := Stay (Two l p r)
             |(Sprout l1 q l2) := Stay (Three l1 q l2 p r)
             end
-        |ordering.eq := Stay (Two l ⟨k,f (some a)⟩ r)
-        |ordering.gt :=
+        else if i = k then Stay (Two l ⟨k,f (some a)⟩ r)
+        else -- i > k
             match modify_raw r with
             |(Stay r) := Stay (Two l p r)
             |(Sprout r1 q r2)  := Stay (Three l p r1 q r2)
             end
-        end
     |(n+1) (Three l (p1@⟨k1,a1⟩) m (p2@⟨k2,a2⟩) r) :=
-        match cmp_using lt i k1 with
-        |ordering.lt :=
+        if i < k1 then 
             match modify_raw l with
             |(Stay l) := Stay (Three l p1 m p2 r)
             |(Sprout l1 q l2) := Sprout (Two l1 q l2) p1 (Two m p2 r)
             end
-        |ordering.eq :=
-            Stay (Three l ⟨k1, f $ some a1⟩ m p2 r) 
-        |ordering.gt := 
-            match cmp_using lt i k2 with
-            |ordering.lt :=
-                match modify_raw m with
-                |(Stay m) := Stay (Three l p1 m p2 r)
-                |(Sprout m1 q m2) := Sprout (Two l p1 m1) q (Two m2 p2 r)
-                end
-            |ordering.eq := Stay (Three l p1 m ⟨k2, f $ some a2⟩ r)
-            |ordering.gt :=
-                match modify_raw r with
-                |(Stay r) := Stay (Three l p1 m p2 r)
-                |(Sprout r1 q r2) := Sprout (Two l p1 m) p2 (Two r1 q r2)
-                end
+        else if i = k1 then Stay (Three l ⟨k1, f $ some a1⟩ m p2 r)
+        else if i < k2 then 
+            match modify_raw m with
+            |(Stay m) := Stay (Three l p1 m p2 r)
+            |(Sprout m1 q m2) := Sprout (Two l p1 m1) q (Two m2 p2 r)
             end
-        end
+        else if i = k2 then Stay (Three l p1 m ⟨k2, f $ some a2⟩ r)
+        else -- i > k2
+            match modify_raw r with
+            |(Stay r) := Stay (Three l p1 m p2 r)
+            |(Sprout r1 q r2) := Sprout (Two l p1 m) p2 (Two r1 q r2)
+            end
     def grown : Type u -> Type u -> Π (n:ℕ), Type u := λ k a n, Σ (growth : ℕ), node k α (n + growth)
     def modify (i : k) (f : option α -> α) {n} (t : node k α n) : grown k α n
-    := match modify_raw lt i f t with
+    := match modify_raw i f t with
        |(Stay n) := sigma.mk 0 n
        |(Sprout l q r) := sigma.mk 1 (Two l q r)
        end
-
-    def set (i : k) (a : α) {n} (t : node k α n) : grown k α n := modify lt i (λ _, a) t
+    def set (i : k) (a : α) {n} (t : node k α n) : grown k α n := modify i (λ _, a) t
 end
 
 section --deletion
-    private def compare : option k -> (k × α) -> ordering
-    |none ⟨k2,_⟩ := ordering.lt
-    |(some k1) ⟨k2,_⟩ := cmp_using lt k1 k2
-    private def if_eq {β : Type u} : ordering -> β -> β -> β
-    |ordering.eq x y := x
-    |_ x y := y
     inductive shrinkage : Type u -> Type u -> ℕ -> Type (u+1)
     |Fail {k} {α} {n} : shrinkage k α n
     |Go {k} {α} {n}: k × α -> node k α (n) -> shrinkage k α (n+1)
@@ -178,20 +166,13 @@ section --deletion
     |(none) (1) (Two Empty p Empty) := Go p Empty
     |(none) (1) (Three Empty p Empty q Empty) := Stop p (Two Empty q Empty)
     |(some k1) (1) (Two Empty (p@⟨k2,a⟩) Empty) := 
-        match cmp_using lt k1 k2 with
-        |ordering.eq := Go p  Empty
-        |_ := Fail
-        end
+        if k1 = k2 then Go p Empty else Fail
     |(some k1) (1) (Three Empty (p2@⟨k2,a2⟩) Empty (p3@⟨k3,a3⟩) Empty ) :=
-        match (⟨cmp_using lt k1 k2, cmp_using lt k2 k3⟩ : ordering × ordering) with
-        |⟨ordering.eq, _⟩ := Stop p2 (Two Empty p3 Empty)
-        |⟨_, ordering.eq⟩ := Stop p3 (Two Empty p2 Empty)
-        |_ := Fail
-        end
-    |ko (n+2) (Two l p r) :=
-        match compare lt ko p with
-        |ordering.lt :=
-            match del ko (n+1) l with
+        if k1 = k2 then Stop p2 (Two Empty p3 Empty)
+        else if k1 = k3 then Stop p3 (Two Empty p2 Empty)
+        else Fail
+    |none (n+2) (Two l p r) :=
+            match del none (n+1) l with
             |Fail := Fail
             |(Stop p2 l) := Stop p2 (Two l p r)
             |(Go p2 l) :=
@@ -200,23 +181,43 @@ section --deletion
                 |(Three rl rp rm rq rr) := Stop p2 (Two (Two l p rl) rp (Two rm rq rr))
                 end
             end
-        |ord :=
-            match del (if_eq ord none ko) (n+1) r with
+    |(some k1) (n+2) (Two l (p@⟨k2,_⟩) r) :=
+        if k1 < k2 then
+            match del (some k1) (n+1) l with
             |Fail := Fail
-            |Stop p2 r := Stop p2 (Two l (if_eq ord p2 p) r)
-            |Go p2 r :=
-                match l with
-                |(Two ll lp lr) := Go p2 (Three ll lp lr (if_eq ord p2 p) r)
-                |(Three ll lp lm lq lr) := Stop p2 (Two (Two ll lp lm) lq (Two lr (if_eq ord p2 p) r))
+            |(Stop p2 l) := Stop p2 (Two l p r)
+            |(Go p2 l) :=
+                match r with
+                |(Two rl rp rr) := Go p2 (Three l p rl rp rr)
+                |(Three rl rp rm rq rr) := Stop p2 (Two (Two l p rl) rp (Two rm rq rr))
                 end
             end
+        else 
+            match del (if k1 = k2 then none else some k1) (n+1) r with
+            |Fail := Fail
+            |Stop p2 r := Stop p2 (Two l (if k1 = k2 then p2 else p) r)
+            |Go p2 r :=
+                let p' := if k1 = k2 then p2 else p in
+                match l with
+                |(Two ll lp lr) := Go p2 (Three ll lp lr p' r)
+                |(Three ll lp lm lq lr) := Stop p2 (Two (Two ll lp lm) lq (Two lr p' r))
+                end
+            end
+    |none (n+2) (Three l p m q r) :=
+        match del none (n+1) l with
+            |Fail := Fail
+            |Stop p2 l := Stop p2 (Three l p m q r)
+            |Go p2 l :=
+                match (⟨m, r⟩ : _ × _) with
+                |⟨(Two ml mp mr), (Two _ _ _ )⟩ := Stop p2 (Two (Three l p ml mp mr) q r)
+                |⟨(Three ml mp mm mq mr), _⟩ := Stop p2 (Three (Two l p ml) mp (Two mm mq mr) q r)
+                |⟨(Two ml mp mr), (Three rl rp rm rq rr)⟩ := Stop p2 (Three (Two l p ml) mp (Two mr q rl) rq (Two rm rq rr))
+                end
         end
-    |ko (n+2) (Three l p m q r) :=
-        match compare lt ko q with
-        |ordering.lt :=
-            match compare lt ko p with
-            |ordering.lt :=
-                match del ko (n+1) l with
+    |(some k1) (n+2) (Three l (p@⟨kp,_⟩) m (q@⟨kq,_⟩) r) :=
+        if k1 < kq then
+            if k1 < kp then
+                match del (some k1) (n+1) l with
                 |Fail := Fail
                 |Stop p2 l := Stop p2 (Three l p m q r)
                 |Go p2 l :=
@@ -226,30 +227,30 @@ section --deletion
                     |⟨(Two ml mp mr), (Three rl rp rm rq rr)⟩ := Stop p2 (Three (Two l p ml) mp (Two mr q rl) rq (Two rm rq rr))
                     end
                 end
-            |ord :=
-                match del (if_eq ord none ko) (n+1) m with
+            else
+                match del (if k1 = kp then none else some k1) (n+1) m with
                 |Fail := Fail
-                |Stop p2 m := Stop p2 (Three l (if_eq ord p2 p) m q r)
+                |Stop p2 m := Stop p2 (Three l (if k1 = kp then p2 else p) m q r)
                 |Go p2 m :=
+                    let p' := if k1 = kp then p2 else p in
                     match ( ⟨l,r⟩ : _ × _  )with
-                    |⟨(Two ll lp lr), (Two _ _ _)⟩ := Stop p2 (Two (Three ll lp lr (if_eq ord p2 p) m) q r)
-                    |⟨(Three ll lp lm lq lr), _⟩ := Stop p2 (Three (Two ll lp lr) lq (Two lr (if_eq ord p2 p) m) q r)
-                    |⟨_, (Three rl rp rm rq rr) ⟩ := Stop p2 (Three l (if_eq ord p2 p) (Two m q rl) rp (Two rm rq rr)) 
+                    |⟨(Two ll lp lr), (Two _ _ _)⟩ := Stop p2 (Two (Three ll lp lr p' m) q r)
+                    |⟨(Three ll lp lm lq lr), _⟩ := Stop p2 (Three (Two ll lp lr) lq (Two lr p' m) q r)
+                    |⟨_, (Three rl rp rm rq rr) ⟩ := Stop p2 (Three l p' (Two m q rl) rp (Two rm rq rr)) 
                     end
                 end
-            end
-        |ord :=
-            match del (if_eq ord none ko) (n+1) r with
+        else
+            match del (if k1 = kq then none else some k1) (n+1) r with
             |Fail := Fail
-            |Stop q2 r := Stop q2 (Three l p m (if_eq ord q2 q) r)
+            |Stop q2 r := Stop q2 (Three l p m (if k1 = kq then q2 else q) r)
             |Go q2 r :=
+                let q' := if k1 = kq then q2 else q in
                 match (⟨l,m⟩ : _ × _) with
-                |⟨Two _ _ _, Two ml mp mr⟩ := Stop q2 (Two l p (Three ml mp mr (if_eq ord q2 q) r))
-                |⟨_, Three ml mp mm mq mr⟩ := Stop q2 (Three l p (Two ml mp mm) mq (Two mr (if_eq ord q2 q) r))
-                |⟨Three ll lp lm lq lr, Two ml mp mr⟩ := Stop q2 (Three (Two ll lp lm) lq (Two lr p ml) mp (Two mr (if_eq ord q2 q) r))
+                |⟨Two _ _ _, Two ml mp mr⟩ := Stop q2 (Two l p (Three ml mp mr q' r))
+                |⟨_, Three ml mp mm mq mr⟩ := Stop q2 (Three l p (Two ml mp mm) mq (Two mr q' r))
+                |⟨Three ll lp lm lq lr, Two ml mp mr⟩ := Stop q2 (Three (Two ll lp lm) lq (Two lr p ml) mp (Two mr q' r))
                 end
             end
-        end
 
     def delete_type (k : Type u) (α : Type u) : ℕ -> Type _
     |0 := node k α 0 -> Σ (shrank : bool), node k α 0
@@ -258,13 +259,20 @@ section --deletion
     def delete (i : k) : Π (n : ℕ), delete_type k α n 
     |(0) := λ h : node k α 0, ⟨ff,h⟩
     |(n+1) := λ t : node k α (n+1), 
-        match del lt (some i) (n+1) t with
+        match del (some i) (n+1) t with
         |Fail := ⟨ff, t⟩
         |Stop _ t := ⟨ff, t⟩
         |Go _ t := ⟨tt, t⟩
         end
 end
 end node
+
+def tree (k : Type u) [decidable_linear_order k] (α : Type u) (n : ℕ) := {t : node k α n // node.is_ordered t}
+namespace tree
+variables {k : Type u} [decidable_linear_order k] (α : Type u)
+def empty : tree k α 0 := ⟨node.empty, by simp * ⟩
+
+end tree
 
 -- TODO: proof that it's ordered
 -- TODO: proof that modify and delete do the right thing.
